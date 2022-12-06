55a16e2c99ebc3290a6d43d0299c087016e52854
Justin Gabriel <gabriel.justin78@gmail.com>
2022-12-06 14:28:32 +0530

    task-33: is else required -functions

---
+++ 11.functions/task-33/else.js
@@ -0,0 +1,21 @@
+/*
+function checkAge(age) {
+    if (age > 18) {
+      return true;
+    } else {
+      // ...
+      return confirm('Did parents allow you?');
+    }
+  }
+*/
+/*
+function checkAge(age) {
+  if (age > 18) {
+    return true;
+  }
+  // ...
+  return confirm('Did parents allow you?');
+}
+*/
+
+// There is no difference in the behaviour of both the function
