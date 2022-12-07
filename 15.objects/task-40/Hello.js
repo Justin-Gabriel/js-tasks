a91b72f82ec33f012aa48d2b9e416f991865f32e
Justin Gabriel <gabriel.justin78@gmail.com>
2022-12-07 15:14:15 +0530

    task-40: one line code for each action -objects

---
+++ 15.objects/task-40/Hello.js
@@ -0,0 +1,9 @@
+let user = {}; //Create an empty object user
+
+user.name = "John"; // Add the property name with the value John
+
+user.surname = "Smith"; // Add the property surname with the value Smith
+
+user.name = "Pete"; // Change the value of the name to Pete
+
+delete user.name; //Remove the property name from the object
