773d4b15e0c1bdac07bc67ead69af79fc3ce6ecd
Justin Gabriel <gabriel.justin78@gmail.com>
2022-12-07 20:11:05 +0530

    task-44: string property works or not -methodsOfPrimitive

---
+++ 16.methodsOfPrimitive/task-44/primitives.js
@@ -0,0 +1,13 @@
+/*
+let str = "Hello";
+
+str.test = 5;
+
+alert(str.test);
+
+What do you think, will it work? What will be shown?
+*/
+
+
+// if there is no strict mode - undefined
+// if there is strict mode - error
