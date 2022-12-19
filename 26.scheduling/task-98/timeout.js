0ae85ed1c8d163db292ddbae8dcbd3f1d13c7282
Justin Gabriel <gabriel.justin78@gmail.com>
2022-12-19 12:49:01 +0530

    task-98: what is alert going to show -scheduling

---
+++ 26.scheduling/task-98/timeout.js
@@ -0,0 +1,13 @@
+let i = 0;
+
+setTimeout(() => alert(i), 100); // ?
+
+// assume that the time to execute this function is >100ms
+for(let j = 0; j < 100000000; j++) {
+  i++;
+}
+
+// What is alert going to show?
+
+
+//Answer :  i = 100000000
