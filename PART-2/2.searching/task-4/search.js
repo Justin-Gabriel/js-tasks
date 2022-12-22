a081c90c7580aa2493b3f2e6731218e07eb200ec
Justin Gabriel <gabriel.justin78@gmail.com>
2022-12-22 22:17:32 +0530

    task-4: search for element -searching

---
+++ PART-2/2.searching/task-4/search.js
@@ -0,0 +1,8 @@
+let table = document.getElementById("age-table");
+console.log(table);
+console.log(table.getElementsByTagName("label"));
+console.log(table.rows[0].cells[0]);
+let doc = document.getElementsByName('search')[0];
+console.log(doc);
+console.log(document.getElementsByName('search')[1]);
+console.log(doc.lastElementChild);
